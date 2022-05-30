e10c99ce4cbf4b6e48b630f7ae7406c3b3348615
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 15:09:28 +0300

    registering socials model with admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Social
 
-# Register your models here.
+
+@admin.register(Social)
+class SocialAdmin(admin.ModelAdmin):
+    fieldset = (None, {"fields": ("social_name", "username")})
+    list_display = ("social_name", "username", "social_url", "created_at")
+    list_filter = ("created_at",)
