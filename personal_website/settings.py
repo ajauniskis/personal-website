b2ff5cbffb508d34d8312cf1ff02cb4e082e3922
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 13:28:06 +0300

    registering whitenoise

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "personal_website.urls"
