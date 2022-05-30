17a65f9e9765abcbd922b00e4c40e856f07b0069
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 15:09:12 +0300

    adding socials model test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from api.models import Social
 
-# Create your tests here.
+
+class SocialModelTest(TestCase):
+    def setUp(self):
+        Social.objects.create(social_name="domain.com/", username="user")
+
+    def test_social_url(self):
+        facebook = Social.objects.get(social_name="domain.com/")
+        expected = facebook.social_name + facebook.username
+        actual = facebook.social_url()
+
+        self.assertEqual(expected, actual)
