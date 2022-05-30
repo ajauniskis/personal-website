68d9b9896bf1ac88889a66a571ac245fd5423832
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 22:41:28 +0300

    update SocialModelTest

--- api/tests.py
+++ api/tests.py
@@ -4,11 +4,11 @@ from api.models import Social
 
 class SocialModelTest(TestCase):
     def setUp(self):
-        Social.objects.create(social_name="domain.com/", username="user")
+        Social.objects.create(social_name="facebook", username="user")
 
     def test_social_url(self):
-        facebook = Social.objects.get(social_name="domain.com/")
-        expected = facebook.social_name + facebook.username
+        facebook = Social.objects.get(social_name="facebook")
+        expected = "facebook.com/" + facebook.username
         actual = facebook.social_url()
 
         self.assertEqual(expected, actual)
