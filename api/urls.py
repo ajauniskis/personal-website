ca45c29277edd97bbae8600b663140fe04f70b6d
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 17:02:14 +0300

    adding get-social endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views.index import index
 from .views.social_view import SocialView
+from .views.get_social import GetSocial
 
 urlpatterns = [
     path("", index),
     path("social", SocialView.as_view()),
+    path("get-social", GetSocial.as_view()),
 ]
