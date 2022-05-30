bdb75a016a8c181100301d0f586944764c33c5ee
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 16:46:04 +0300

    change choices

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 # Create your models here.
 class Social(Model):
-    social_choices = (("facebook.com/", "facebook"),)
+    social_choices = (("facebook.com", "facebook"),)
 
     social_name = CharField(
         max_length=100,
@@ -16,7 +16,7 @@ class Social(Model):
     created_at = DateTimeField(auto_now_add=True, null=False, blank=False)
 
     def __str__(self):
-        return self.social_name + self.username
+        return self.social_name + "/" + self.username
 
     def social_url(self):
-        return self.social_name + self.username
+        return self.social_name + "/" + self.username
