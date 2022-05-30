02ca339a683ac56a7a1cbd373539ec35b6445b2c
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-05-30 15:33:36 +0300

    registering social view url

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views.index import index
+from .views.social_view import SocialView
 
 urlpatterns = [
     path("", index),
+    path("social", SocialView.as_view()),
 ]
