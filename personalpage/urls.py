3de7c425b04ee32f5a9435bc8c163ead41a4c99d
Algirdas Jauniskis <jauniskis.a@gmail.com>
2022-06-14 23:23:52 +0300

    registering contacts view

--- personalpage/urls.py
+++ personalpage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views.index import IndexView
+from .views.contacts import ContactsView
 
 app_name = "personalpage"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("contacts", ContactsView.as_view(), name="contacts"),
 ]
